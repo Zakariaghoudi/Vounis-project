f13a83f172eeb17d989d9c00909f760c0016aae9
zakaria <zakariaghoudi1@gmail.com>
2025-10-16 18:14:41 +0100

    update

---
+++ config/email.js
@@ -0,0 +1,16 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+    const transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.PASS_USER,
+   
+      },
+    });
+
+
+module.exports = transporter;
