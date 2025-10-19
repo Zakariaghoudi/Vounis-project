159675ca2745bd80d11e7196e5c7ab6f23547add
zakaria <zakariaghoudi1@gmail.com>
2025-10-19 13:00:00 +0100

    up: nodemailer

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,22 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-    const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.PASS_USER,
-   
-      },
-    });
-
+const transport = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.PASS_USER,
+  },
+};
+const transporter = nodemailer.createTransport(transport);
+transporter.verify((error, succes) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("success ");
+  }
+});
 
 module.exports = transporter;
